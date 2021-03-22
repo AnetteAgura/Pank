71fb0f7e5a03485a486922d30ca10d42532f7ed4
Anette Aguraiuja <anetteagura@gmail.com>
2021-03-22 11:05:43 +0200

    Tried, but not working.

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,27 @@
+<script>
+	let username;
+	let password;
+	function loginHandler() {
+	}
+</script>
+
 <svelte:head>
-	<title>About</title>
+	<title>Log in</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<h1>Log in</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<form id="test" action="#" method="post">
+	<div class="form-group">
+		<label for="username">Username</label>
+		<input bind:value={username} class="form-control" type="text" name="username" id="username" />
+	</div>
+	<div class="form-group">
+		<label for="password">Password</label>
+		<input bind:value={password} class="form-control" type="password" name="password" id="password" />
+	</div>
+	<p>
+		<input type="submit" value="Login" class="btn btn-primary btn-block" on:click={loginHandler}/>
+	</p>
+</form>
+<p>Don't have an account yet? Create one <a href="./register-user">here!</a></p>
---
+++ src/routes/register-user.svelte
@@ -0,0 +1,46 @@
+<!-- register.svelte -->
+<script>
+    let name;
+    let username;
+    let password;
+
+    function doPost() {
+
+        fetch("https://barbank.diarainfra.com/sessions", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name,
+                username,
+                password
+            })
+        })
+    }
+
+</script>
+
+<svelte:head>
+    <title>Register</title>
+</svelte:head>
+
+<h1>Register</h1>
+
+<form id="test" action="#" method="post">
+    <div class="form-group">
+        <label for="name">Full name</label>
+        <input bind:value={name} class="form-control" type="text" name="name" id="name" />
+    </div>
+    <div class="form-group">
+        <label for="username">Username</label>
+        <input bind:value={username} class="form-control" type="text" name="username" id="username" />
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input bind:value={password} class="form-control" type="password" name="password" id="password" />
+    </div>
+    <p>
+        <input type="submit" value="Register" class="btn btn-primary btn-block" on:click={doPost}>
+    </p>
+</form>
