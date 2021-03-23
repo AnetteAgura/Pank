61ab99f5b8a91181e2db7427ac2c8f0f486cea33
Anette Aguraiuja <anetteagura@gmail.com>
2021-03-23 14:22:10 +0200

    As a user I can register to site [Delivers #177415553]

---
+++ src/routes/auth/register.js
@@ -0,0 +1,13 @@
+import * as api from 'api.js';
+
+export function post(req, res) {
+    api.post('users', req.body).then(response => {
+        if (response.user) {
+            req.session.user = response.user;
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+
+        res.end(JSON.stringify(response));
+    });
+}
