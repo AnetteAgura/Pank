c8d9ac813c3f2a4209ec5f2048144414ce02eff7
Anette Aguraiuja <anetteagura@gmail.com>
2021-04-03 23:04:36 +0300

    As a user I can see my data when I login [Delivers #177558064]

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,8 +1,9 @@
 <script>
 	export let segment;
-	import { goto, stores } from '@sapper/app';
-	import { post } from 'utils.js';
-	const { page, session } = stores();
+	import {goto, stores} from '@sapper/app';
+	import {post} from 'utils.js';
+
+	const {page, session} = stores();
 
 	async function logout() {
 		await post(`auth/logout`);
@@ -13,43 +14,37 @@
 
 <style>
 	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
+		border-bottom: 1px solid rgba(255, 62, 0, 0.1);
 		font-weight: 300;
 		padding: 0 1em;
 	}
-
 	ul {
 		margin: 0;
 		padding: 0;
 	}
-
 	/* clearfix */
 	ul::after {
 		content: '';
 		display: block;
 		clear: both;
 	}
-
 	li {
 		display: block;
 		float: left;
 	}
-
 	[aria-current] {
 		position: relative;
 		display: inline-block;
 	}
-
 	[aria-current]::after {
 		position: absolute;
 		content: '';
 		width: calc(100% - 1em);
 		height: 2px;
-		background-color: rgb(255,62,0);
+		background-color: rgb(255, 62, 0);
 		display: block;
 		bottom: -1px;
 	}
-
 	a {
 		text-decoration: none;
 		padding: 1em 0.5em;
@@ -60,10 +55,12 @@
 <nav>
 	<ul>
 		{#if $session.token}
-			<li><a aria-current="{segment === 'logout' ? 'page' : undefined}" href="{logout}" on:click|preventDefault={logout}>Log out</a></li>
-			{:else}
+			<li><a aria-current="{segment === 'overview' ? 'page' : undefined}" href="/overview">Overview</a></li>
+			<li><a aria-current="{segment === 'logout' ? 'page' : undefined}" href="{logout}"
+				   on:click|preventDefault={logout}>Log out</a></li>
+		{:else}
 			<li><a aria-current="{segment === 'login' ? 'page' : undefined}" href="login">Log in</a></li>
 			<li><a aria-current="{segment === 'register' ? 'page' : undefined}" href="register">Register</a></li>
-			{/if}
+		{/if}
 	</ul>
 </nav>
